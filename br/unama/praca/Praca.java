975dc98d7002e7b4e04ddbdee810058f0a164497
Teteuww234 <matheuscabralrod@gmail.com>
2024-09-06 13:58:48 +0000

    bleu

--- br/unama/praca/Praca.java
+++ br/unama/praca/Praca.java
@@ -3,8 +3,8 @@ package br.unama.praca;
 public class Praca {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa();
-        p1.setNome ("Suzane Vitória O. Rodrigues");
-        p1.setCpf("001.624.112-60");
+        p1.setNome ("María Falcantino Letícia");
+        p1.setCpf("089.567.455-70");
 
         System.out.println("Nome: " + p1.getNome());
         System.out.println("CPF: " + p1.getCpf());
